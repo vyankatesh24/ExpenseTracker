a247c4e9b3f007d1548139fef7faf3c2e3b7bc21
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2018-12-13 02:18:29 +0530

    added implementation for delete button

--- app/src/main/java/com/vyankatesh/expensetracker/adapter/CategoriesAdapter.java
+++ app/src/main/java/com/vyankatesh/expensetracker/adapter/CategoriesAdapter.java
@@ -1,15 +1,19 @@
 package com.vyankatesh.expensetracker.adapter;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vyankatesh.expensetracker.R;
 import com.vyankatesh.expensetracker.database.Categories;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -18,11 +22,23 @@ import butterknife.ButterKnife;
 public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.ViewCategoryHolder> {
 
     private List<Categories> mCategoriesList;
-
-    public CategoriesAdapter(List<Categories> mCategoriesList) {
-        this.mCategoriesList = mCategoriesList;
+    public interface OnItemClickListener{
+        void onItemClicked(int position, int itemID);
     }
 
+    private OnItemClickListener listener;
+
+    public CategoriesAdapter(List<Categories> mCategoriesList, OnItemClickListener listener) {
+        this.listener = listener;
+        this.mCategoriesList = mCategoriesList;
+
+    }
+
+    public void setItems(List<Categories> list){
+        mCategoriesList.clear();
+        mCategoriesList.addAll(list);
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
@@ -34,6 +50,10 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
 
     @Override
     public void onBindViewHolder(@NonNull ViewCategoryHolder holder, int i) {
+
+        holder.itemView.setOnClickListener(onclick -> {
+            listener.onItemClicked(i, mCategoriesList.get(i).getId());
+        });
         holder.mTextCategory.setText(mCategoriesList.get(i).getCategory());
     }
 
--- app/src/main/java/com/vyankatesh/expensetracker/fragment/MainTab3.java
+++ app/src/main/java/com/vyankatesh/expensetracker/fragment/MainTab3.java
@@ -1,30 +1,35 @@
 package com.vyankatesh.expensetracker.fragment;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.vyankatesh.expensetracker.R;
 import com.vyankatesh.expensetracker.adapter.CategoriesAdapter;
 import com.vyankatesh.expensetracker.database.AppDatabase;
 import com.vyankatesh.expensetracker.database.Categories;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainTab3 extends Fragment {
+public class MainTab3 extends Fragment implements CategoriesAdapter.OnItemClickListener {
 
     @BindView(R.id.list_categories)
     RecyclerView mCategoriesView;
@@ -37,14 +42,38 @@ public class MainTab3 extends Fragment {
         View view = inflater.inflate(R.layout.main_tab3, container, false);
         new LoadCategories().execute();
         ButterKnife.bind(this, view);
+        mCategoriesAdapter = new CategoriesAdapter(new ArrayList<>(0), this);
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mCategoriesView.getContext(),
                 LinearLayoutManager.VERTICAL);
+        mCategoriesView.setAdapter(mCategoriesAdapter);
         mCategoriesView.addItemDecoration(dividerItemDecoration);
         return view;
     }
 
     public void setData() {
         new LoadCategories().execute();
+    }
+
+    @Override
+    public void onItemClicked(int position, int itemID) {
+        Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setPositiveButton("Delete Category", (dialog, which) -> {
+            AppDatabase database = AppDatabase.getDatabase(Objects.requireNonNull(getContext()).getApplicationContext());
+            Log.v("SizeOfList", String.valueOf(mCategoriesList.size()));
+            AsyncTask.execute(() -> database.categoriesDao().deleteCategory(itemID));
+            mCategoriesList.remove(position);
+            Log.v("SizeOfList", String.valueOf(mCategoriesList.size()));
+            new LoadCategories().execute();
+            mCategoriesAdapter.setItems(mCategoriesList);
+        });
+        builder.setNegativeButton("Edit Category", (dialog, which) -> {
+
+        });
+        builder.setTitle("Choose any option");
+        builder.setMessage("");
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -60,8 +89,7 @@ public class MainTab3 extends Fragment {
         @Override
         protected void onPostExecute(Void aVoid) {
             if (mCategoriesList != null && !mCategoriesList.isEmpty()) {
-                mCategoriesAdapter = new CategoriesAdapter(mCategoriesList);
-                mCategoriesView.setAdapter(mCategoriesAdapter);
+                mCategoriesAdapter.setData(mCategoriesList);
             }
             super.onPostExecute(aVoid);
         }
